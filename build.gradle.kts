bc6025005113a96c3503e4718df41223d395e20b
Volodymyr <vmartynyuk@pettersonapps.com>
2022-07-16 11:58:43 +0300

    Setup Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version Versions.gradlePluginAndroid apply false
+    id("com.android.library") version Versions.gradlePluginAndroid apply false
+    id("org.jetbrains.kotlin.android") version Versions.gradlePluginKotlin apply false
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+    const val kotlin = "1.7.10"
+    const val androidxCoreKts = "1.8.0"
+    const val androidxAppCompat = "1.4.2"
+    const val androidxConstraintLayout = "2.1.4"
+    const val googleMaterial = "1.6.1"
+
+    const val gradlePluginAndroid = "7.2.1"
+    const val gradlePluginKotlin = kotlin
+}
